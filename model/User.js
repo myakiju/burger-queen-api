7a78843b1b6ce9579f4f2f1a696bb8b97bdf0cb4
Paloma Araujo <paclaraujo@gmail.com>
2024-02-24 21:14:28 -0300

    feat: add user model

---
+++ model/User.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'waiter', 'chef'],
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const User = model('User', userSchema);
+
+module.exports = { User };
