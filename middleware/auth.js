52e72d96762c9131ba4e0f01a6a4a9bc4c363e6e
Paloma Araujo <paclaraujo@gmail.com>
2024-03-10 15:57:18 -0300

    refactor: add req userId and userRole into auth validations

--- middleware/auth.js
+++ middleware/auth.js
@@ -23,29 +23,24 @@ module.exports = (secret) => (req, resp, next) => {
 
     if (!user) resp.status(404).json({ message: 'Usuário não encontrado.' });
 
-    next();
+    req.userId = decodedToken.uid;
+    req.userRole = decodedToken.role;
 
-    // TODO: Verify user identity using `decodeToken.uid`
+    next();
   });
 };
 
-module.exports.isAuthenticated = (req) => (
-  // TODO: Decide based on the request information whether the user is authenticated
-  true
-);
+module.exports.isAuthenticated = (req) => !!req.userId;
 
-module.exports.isAdmin = (req) => (
-  // TODO: Decide based on the request information whether the user is an admin
-  true
-);
+module.exports.isAdmin = (req) => req.userRole === 'admin';
 
-module.exports.requireAuth = (req, resp, next) => (
+module.exports.requireAuth = (req, _, next) => (
   (!module.exports.isAuthenticated(req))
     ? next(401)
     : next()
 );
 
-module.exports.requireAdmin = (req, resp, next) => (
+module.exports.requireAdmin = (req, _, next) => (
   // eslint-disable-next-line no-nested-ternary
   (!module.exports.isAuthenticated(req))
     ? next(401)
